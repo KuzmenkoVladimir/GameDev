32793b165989aa145eea2b4266c554a28659335d
Oleg <oleg@mail.com>
2019-10-26 16:55:02 +0300

    Startproject

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public GameObject cameraPlayer;
+    public Vector3 cameraPos;
+
+
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+        Vector3 position = new Vector3(Input.GetAxis("Horizontal") * speed, 0, 0);
+        transform.position += position;
+    }
+
+    void LateUpdate()
+    {
+        //cameraPlayer.transform.position = transform.position + cameraPos;
+
+        cameraPlayer.transform.position = Vector3.SmoothDamp(cameraPlayer.transform.position, transform.position + cameraPos, ref velocity, smoothTime);
+    }
+
+    
+}
---
+++ Assets/Script/StairsTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairsTrigger : MonoBehaviour
+{
+    public GameObject player;
+    public float timeWait;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+          StartCoroutine(WaitAndPrint(2.0F));
+        }
+    }
+
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        player.transform.position += Vector3.up * 5;
+        
+    }
+}
